6355dc68b8125f27e95f1a5e9d5a0d84803684ed
adriano.santos <adriano.santos@squadra.com.br>
2019-06-03 10:36:12 -0300

    exercicio 02 parte 14

---
+++ exercicio 02 parte 14/exercicio 02 parte 14/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_02_parte_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Digite o primeiro número.");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número.");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro número.");
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c && b > c)
+            {
+                Console.WriteLine(a + ", " + b + ", " + c);
+              
+            }
+
+            else if (a > b && a > c && c > b)
+            {
+                Console.WriteLine(a + ", " + c + ", " + b);
+            }
+
+            else if (b > a && b > c && a > c)
+            {
+                Console.WriteLine(b + ", " + a + ", " + c);
+            }
+
+            else if (b > a && b > c && c > a)
+            {
+                Console.WriteLine(b + ", " + c + ", " + a);
+            }
+
+            else if (c > a && b > c && a > b)
+            {
+                Console.WriteLine(c + ", " + a + ", " + b);
+            }
+
+            else if (c > a && b > c && b > a)
+            {
+                Console.WriteLine(c + ", " + b + ", " + a);
+            }
+               
+        }
+    }
+}
+
